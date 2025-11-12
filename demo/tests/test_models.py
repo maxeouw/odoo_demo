0b4062d5278ff31cb25e58951eaa1567f6111cb0
edwardgay <e.gay@students.ephec.be>
2025-11-12 15:32:52 +0100

    [Fix] test_fix

--- demo/tests/test_models.py
+++ demo/tests/test_models.py
@@ -29,7 +29,7 @@ class TestUsers(TransactionCase):
                     'name': 'Marie-Noël',
                     'login': 'mnv',
                 })
-                user.description = None
+            self.assertEqual(user.description,  None)
 
 
     def test_description_one_line(self):
